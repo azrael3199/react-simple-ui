e0b319d7c65f28f9e72654919b24c8df9b1c3aa9
spawar_pr(Shubham Pawar) <spawar_pr@medable.com>
2022-05-30 22:05:10 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1 className="text-center text-xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
