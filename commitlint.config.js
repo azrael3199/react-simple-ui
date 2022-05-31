d156b94795e3e3a773a23489268f419acf2c1fd1
Chinmay Ghungurde <54586237+azrael3199@users.noreply.github.com>
2022-05-31 21:36:48 +0530

    chore(RSU-12): initialized commitlint and commitzen, migrated broken husky config (#2)
    * chore(RSU-12): initialized commitlint and commitzen, migrated broken husky config
    
    * chore: RSU-12 changed commitlint message rules to allow cases
    
    * chore: RSU-12 changed commitlint message rules to allow cases

---
+++ commitlint.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'improvement',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'subject-case': [
+      1,
+      'always',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+  },
+};
